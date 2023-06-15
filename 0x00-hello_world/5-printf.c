bd6819acd062ba3a6d1850830c5c42bf4d123a90
yousef1412 <yousefabdelsalam1412@gmail.com>
2023-06-15 14:34:37 -0700

    finished

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - main block
  * Return: 0
-*/
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
