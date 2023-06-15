6ba667d1e252d9ab95c491a7728f1246827d130c
yousef1412 <yousefabdelsalam1412@gmail.com>
2023-06-15 13:58:47 -0700

    finished

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - entry poit
- * return: always 0 (success)
+ * main - entry point
+ * Return: always 0 (success)
  */
+
 int main(void)
 {
-	puts(*\"Programming is like building a multilingual puzzle");
-	return(0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }	
