55b527ce2a50c25f09dbeac2d552cbd1205f5c40
yousef1412 <yousefabdelsalam1412@gmail.com>
2023-06-19 14:05:38 -0700

    finished

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		for (m = n + 1 ; m < 10 ; m++)
+		{
+			putchar(n + '0');
+			putchar(m + '0');
+
+
+			if (n != 8 || m != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
