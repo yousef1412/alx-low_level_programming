b9fefb6ffbc648a31d25bb0f3dbcbeaebfd821e7
yousef1412 <yousefabdelsalam1412@gmail.com>
2023-06-27 08:25:55 -0700

    finished

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a sring follwed by a new line for stdout
+ *
+ * @str: srting parameter to print
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	while(*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
