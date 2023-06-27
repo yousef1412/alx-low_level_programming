7bdc8a4127bb0ff1ccef361968915efcd5414694
yousef1412 <yousefabdelsalam1412@gmail.com>
2023-06-27 08:09:39 -0700

    finished

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _sterlen - return the length of the string
+ * _strlen - return the length of the string
  *
  * @s: string parameter input
  *
- * return: then length of the string
+ * return: length of the string
  */
 
 int _strlen(char *s)
@@ -12,6 +12,7 @@ int _strlen(char *s)
 	int counter;
 
 	for (counter = 0; *s != '\0'; s++)
+
 		++counter;
 	return (counter);
 }
