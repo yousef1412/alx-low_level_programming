1fdf6201891a7a66b1054aef9d34667b6702b53c
yousef1412 <yousefabdelsalam1412@gmail.com>
2023-08-30 01:07:45 -0700

    finished

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+/**
  * main - main block
  * Return: 0
 */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle")
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
