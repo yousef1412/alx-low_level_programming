08e58af95de601b8ff4a7307b391dbc226b6f704
yousef1412 <yousefabdelsalam1412@gmail.com>
2023-08-30 16:02:18 -0700

    finished

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  *main - block main
- *Return: 1
+ *Return:1
 */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
