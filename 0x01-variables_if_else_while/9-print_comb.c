c6f6038a2a8d3be2292fa9d2ad174b02a009ad73
yousef1412 <yousefabdelsalam1412@gmail.com>
2023-09-08 11:21:28 +0300

    finished

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,13 @@ int main(void)
 int n;
 for (n = 0; n <= 9; n++)
 {
-	if (n != 9) 
-	{
 	putchar (n + '0');
+	if (n != 9)
+	{
 	putchar (',');
 	putchar (' ');
 	}
-	else 
-	{
-	putchar (n + '0');
-	}
 }
+putchar ('\n');
 return (0);
 }
