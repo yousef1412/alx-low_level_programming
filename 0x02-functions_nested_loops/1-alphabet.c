f251b9480edb54d247ac216df365ba1a98aa2fad
yousef1412 <yousefabdelsalam1412@gmail.com>
2023-09-11 11:22:19 +0300

    finished

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - function that print alphabet
+ *
+ * main - main block
+ *
+ * Return: no return
+*/
+void print_alphabet(void)
+{
+int n;
+	for (n = 'a'; n <= 'z'; n++)
+	{
+	putchar(n);
+	putchar('\n')
+	}
+}
+
